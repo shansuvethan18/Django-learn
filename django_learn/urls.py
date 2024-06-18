78642a430e74f5497463eaf3c1c4976040de02ec
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-18 20:32:12 +0530

    test app creation and view new content

--- django_learn/urls.py
+++ django_learn/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path ('playground/', include('playground.urls')),
+    path ("" , include('test_app.urls')),
      path("__debug__/", include("debug_toolbar.urls")),
 ]
