e7dc0fe66cdbe1b9d02a18c415a7841eeaf4d4e5
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-19 23:42:59 +0530

    test 1(ii)

--- test_app/views.py
+++ test_app/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, Welcome to the world of Django!")
+    return render(request, 'index.html')
