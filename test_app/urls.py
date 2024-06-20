7abb794de68a2a76587a4e1542aa582460f73337
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-20 17:30:01 +0530

    templates and dynamic routes

--- test_app/urls.py
+++ test_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path("home/", views.home, name="home" ),
-    path("room/", views.room, name="room" )
+    path("room/<int:pk>/", views.room, name="room" )
 ]
--- test_app/views.py
+++ test_app/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id':1,'name':'Lets learn Python!'},
+    {'id':2,'name':'Design!'},
+    {'id':3,'name':'Lets learn Java!'}
+]
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'test_app/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+            context = {'room':room}
+
+    return render(request, 'test_app/room.html',context)
