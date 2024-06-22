3ae654a49784b784c7c6ad2328cb39dffced27a4
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-22 23:20:47 +0530

    search functionality part-3

--- test_app/views.py
+++ test_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
+from django.db.models import Q
+
 # rooms = [
 #     {'id':1,'name':'Lets learn Python!'},
 #     {'id':2,'name':'Design!'},
@@ -11,9 +13,15 @@ from .forms import RoomForm
 # Create your views here.
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(topic__name__icontains=q) 
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+    Q(description__icontains=q) 
+    ) 
     topics = Topic.objects.all()
-    context = {'rooms':rooms, 'topics': topics}
+    room_count= rooms.count()
+
+    context = {'rooms':rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'test_app/home.html', context)
 
 def room(request,pk):
