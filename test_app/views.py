3db21e6e4321e695f2e8131404543fe8c2b0ac9a
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-23 00:51:53 +0530

    Login p-2 & logout

--- test_app/views.py
+++ test_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
 
 # rooms = [
 #     {'id':1,'name':'Lets learn Python!'},
@@ -13,8 +16,29 @@ from django.db.models import Q
 # Create your views here.
 
 def loginPage(request):
+    if request.method == 'POST':
+        username= request.POST.get('username')
+        password =  request.POST.get('password')
+
+        try:
+            user= User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist.")
+
+        user= authenticate(request,username=username, password=password)
+
+        if user is not None:
+            login (request,user)
+            return redirect ('home')
+        else:
+            messages.error(request, "Username or Password is incorrect.")
+
     context ={}
     return render(request, 'test_app/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect ('home')
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
