9ad05076c9b0655e042f7d7ef817440ffc884995
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-23 14:27:49 +0530

    Restricted Pages

--- test_app/views.py
+++ test_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Room,Topic
 from .forms import RoomForm
@@ -16,6 +17,9 @@ from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 
 def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
     if request.method == 'POST':
         username= request.POST.get('username')
         password =  request.POST.get('password')
@@ -63,6 +67,7 @@ def room(request,pk):
 
     return render(request, 'test_app/room.html',context)
 
+@login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
     if request.method =='POST':
@@ -75,13 +80,19 @@ def createRoom(request):
     context={'form':form}
     return render(request, 'test_app/room_form.html', context)
 
-
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
+    if request.user != room.host:
+            return HttpResponse('You are not allowed here!')
+
     if request.method == 'POST' :
         form = RoomForm(request.POST, instance=room)
+
+        
+
         if form.is_valid():
             form.save()
             return redirect('home')
@@ -89,9 +100,13 @@ def updateRoom(request, pk):
     context ={'form':form}
     return render(request, 'test_app/room_form.html', context)
 
-
+@login_required(login_url='login')
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
+
+    if request.user != room.host:
+            return HttpResponse('You are not allowed here!')
+    
     if request.method == 'POST':
         room.delete()
         return redirect('home')
