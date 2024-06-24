31f5c0e254f50e427b31e29a92839e77552f6c00
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-25 00:12:01 +0530

    static files p-2 and some Changes in forms

--- test_app/views.py
+++ test_app/views.py
@@ -121,8 +121,10 @@ def createRoom(request):
     if request.method =='POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('home')
+           room =form.save(commit=False)
+           room.host = request.user
+           room.save()
+           return redirect('home')
          
 
     context={'form':form}
