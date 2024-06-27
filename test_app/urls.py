592cfcf7ccfdd653198c1250617f5f62a8be9115
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-27 20:57:41 +0530

    Edit User Profile-1

--- test_app/urls.py
+++ test_app/urls.py
@@ -5,13 +5,14 @@ urlpatterns = [
     path("login/", views.loginPage, name="login" ),
     path("logout/", views.logoutUser, name="logout" ),
     path("register/", views.registerPage, name="register" ),
-    path("home/", views.home, name="home" ),
+    path("", views.home, name="home" ),
     path("room/<int:pk>/", views.room, name="room" ),
     path("profile/<str:pk>", views.userProfile, name="user-profile"),
     path('create-room/', views.createRoom,name="create-room"),
     path('update-room/<str:pk>', views.updateRoom,name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom,name="delete-room"),
     path('delete-message/<str:pk>', views.deleteMessage,name="delete-message"),
+    path('update-user/', views.updateUser,name="update-user"),
 
     path('sample/', views.samplePage,name="sample"),
     path('sample2/', views.samplePage2,name="sample2")
--- test_app/views.py
+++ test_app/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Room,Topic,Message
-from .forms import RoomForm
+from .forms import RoomForm,UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
@@ -198,3 +198,17 @@ def deleteMessage(request, pk):
     
     return render(request, 'test_app/delete.html', {'obj':message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user=request.user
+    form=UserForm(instance=user)
+    if request.method == 'POST':
+        form= UserForm(request.POST, instance=user)
+        if form.is_valid():
+           
+           form.save()
+           return redirect('user-profile',pk=user.id)
+
+
+    context={'form':form}
+    return render(request, 'test_app/update-user.html', context)
