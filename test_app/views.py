18a2290020abfba9916f89786ce1185c9ad27462
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-30 22:51:33 +0530

    Mobile responsiveness-3 (Activity messages)

--- test_app/views.py
+++ test_app/views.py
@@ -73,7 +73,7 @@ def home(request):
         Q(name__icontains=q) |
     Q(description__icontains=q) 
     ) 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count= rooms.count()
     room_messages= Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -219,4 +219,5 @@ def topicsPage(request):
       return render(request, 'test_app/topics.html', {'topics':topics})
 
 def activityPage(request):
-      return render(request, 'test_app/activity.html', {})
+      room_messages = Message.objects.all() [0:2]
+      return render(request, 'test_app/activity.html', {'room_messages':room_messages})
