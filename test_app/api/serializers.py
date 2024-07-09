e48e78f1849485139d433f157c27760e59be494b
shansuvethan18 <shansuvethan5120@gmail.com>
2024-07-09 23:43:54 +0530

    REST api

--- test_app/api/serializers.py
+++ test_app/api/serializers.py
@@ -1,1 +1,8 @@
- 
+from rest_framework.serializers import ModelSerializer
+from test_app.models import Room
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model=Room
+        fields = '__all__'
