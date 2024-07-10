aef34753bdb9937484dbdb5e4cf22ca11711bc00
shansuvethan18 <shansuvethan5120@gmail.com>
2024-07-10 23:59:26 +0530

    completa

--- test_app/views.py
+++ test_app/views.py
@@ -3,10 +3,10 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Room,Topic,Message,User
-from .forms import RoomForm,UserForm
+from .forms import RoomForm,UserForm,MyUserCreationForm
 from django.db.models import Q
 from django.contrib.auth import authenticate,login,logout
-from django.contrib.auth.forms import UserCreationForm
+
 
 # rooms = [
 #     {'id':1,'name':'Lets learn Python!'},
@@ -48,10 +48,10 @@ def logoutUser(request):
 
 def registerPage(request):
      
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form= UserCreationForm(request.POST)
+        form= MyUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -202,7 +202,7 @@ def updateUser(request):
     user=request.user
     form=UserForm(instance=user)
     if request.method == 'POST':
-        form= UserForm(request.POST, instance=user)
+        form= UserForm(request.POST,request.FILES, instance=user)
         if form.is_valid():
            
            form.save()
