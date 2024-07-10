db281d8fa8be54ad9f84632da88a9a67708878a4
shansuvethan18 <shansuvethan5120@gmail.com>
2024-07-11 00:32:17 +0530

    complete project

--- django_learn/settings.py
+++ django_learn/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-epj+l$w$&v6bp533p+@5-6bd7s1k=u0d1lauz#x96*lrf29w^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
